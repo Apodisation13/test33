7e043776674180e1627d2bbcf4c42f972aaf1dc6
Pavel Soloshenko <apodisation13@gmail.com>
2021-02-09 15:44:16 +0300

    init

---
+++ 5.py
@@ -0,0 +1,11 @@
+from summ_def import summ
+from minus_def import minus
+from mul_def import mul
+from div_def import div
+
+
+if __name__ == "__main__":
+    print(summ(*(4, 5, -3)))
+    print(minus([4, 5, -2]))
+    print(mul(range(1, 5)))
+    print(div((36, 6, 3, -2, -1)))
